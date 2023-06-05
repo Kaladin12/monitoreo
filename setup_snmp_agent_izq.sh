c4d25297a4e97eae58bc7ff8406ffc37fa3fc15b
Kaladin12 <eliancruz998@gmail.com>
2023-06-04 22:58:47 -0700

    debian

---
+++ setup_snmp_agent_izq.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update
+apt install -y snmp snmpd
+
+interfaces_file='/etc/network/interfaces'
+
+ip=$(grep -Po "^\s*address \K[\d.]+(?=\/)" "$interfaces_file" | head -n 1)
+
+echo "$ip"
+
+sed -i "s/^agentaddress.*/agentaddress udp:${ip}:161/" /etc/snmp/snmpd.conf
+sed -i 's/^rocommunity.*/rocommunity public-asr 10.21.1.50/' /etc/snmp/snmpd.conf
+
+systemctl restart snmpd
